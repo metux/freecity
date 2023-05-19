8c95a92995c076cec10d139a6da3b83d408605d7
Enrico Weigelt, metux IT consult <info@metux.net>
2023-05-19 13:30:12 +0200

    1

--- src/core/base/dim.go
+++ src/core/base/dim.go
@@ -2,7 +2,6 @@ package base
 
 import (
     "fmt"
-    "image"
     "gopkg.in/yaml.v3"
 )
 
@@ -13,12 +12,6 @@ type Dim struct {
 
 func (d Dim) String() string {
     return fmt.Sprintf("%dx%d", d.Width, d.Height)
-}
-
-func (p Dim) ImagePoint(scale float64) image.Point {
-    return image.Point{
-        int(float64(p.Width) * scale),
-        int(float64(p.Height) * scale)}
 }
 
 func (d Dim) HasPoint(p Point) bool {
