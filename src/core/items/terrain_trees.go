eb80db5d264ad35bd8ad5095e427b1d5ff8038e6
Enrico Weigelt, metux IT consult <info@metux.net>
2023-05-19 16:14:11 +0200

    1

--- src/core/items/terrain_trees.go
+++ src/core/items/terrain_trees.go
@@ -1,25 +1,25 @@
 package items
 
 // just place, w/o billing
-func (tm * TerrainMap) PlaceWood(p point, w byte) {
-    if tile := tm.tileAt(p); tile != nil {
+func (tm * TerrainMap) PlaceWood(p point, w byte) bool {
+    return tm.ModifyTile(p, func (tile * Tile) bool {
         if tile.Wood < w {
             tile.Wood = w
-            tm.TouchTerrain()
+            return true
         }
-    }
+        return false
+    })
 }
 
 // clean zone w/ billing
 func (tm * TerrainMap) CleanWood(act Action, p point) bool {
-    if tile := tm.tileAt(p); tile != nil {
+    return tm.ModifyTile(p, func (tile * Tile) bool {
         if tile.Wood > 0 {
             if tm.trySpendFunds(act, tm.GeneralRules.Costs.Bulldoze, "clean wood") {
                 tile.Wood = 0
-                tm.TouchTerrain()
                 return true
             }
         }
-    }
-    return false
+        return false
+    })
 }
